60de7f116b920785c55cf4aae8c4daaa792d3b58
sandhyashrestha73 <sandhyashrestha431@gmail.com>
2026-05-04 20:48:05 +0545

    initial commit

---
+++ 01-folder/src/App.jsx
@@ -0,0 +1,16 @@
+function App(){
+  return (
+    <>
+      <div id="parents">
+        <h1>Dad</h1>
+        <h1>Mom</h1>
+      </div>
+      <div id="child">
+        <h1>First Born</h1>
+        <h1>Second Born</h1>
+        <h1>Third Born</h1>
+      </div>
+    </>
+  );
+}
+export default App
