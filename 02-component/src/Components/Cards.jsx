c3ca9935841eae892454e82a3e1f72ec45cad64b
sandhyashrestha73 <sandhyashrestha431@gmail.com>
2026-05-06 21:07:02 +0545

    added React components practice

---
+++ 02-component/src/Components/Cards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Cards = () => {
+  return (
+    <div id='card'><h1>Sandhya Shrestha</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus, sint!</p>
+    </div>
+  )
+}
+
+export default Cards
