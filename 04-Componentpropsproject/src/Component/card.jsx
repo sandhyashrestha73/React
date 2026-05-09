ffd4329b37ba44359ca0e5950eca65240b082202
sandhyashrestha73 <sandhyashrestha431@gmail.com>
2026-05-09 13:40:09 +0545

    Projectpropscompleted

--- 04-Componentpropsproject/src/Component/card.jsx
+++ 04-Componentpropsproject/src/Component/card.jsx
@@ -1,28 +1,28 @@
 import React from 'react'
 
-const card = () => {
+const card = (props) => {
   return (
     <div className="card">
      <div>
       <div className='top'>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCw9ADW_e3phAevjk1wGYCyOujxGmT_3cTQQ&s" alt="" />
+          <img src={props.brand} alt="" />
           <button>Save </button>
       </div>
      
       <div className='center'>
-        <h3>Amazon <span>5 days ago</span></h3>
-        <h4>Senior UI/UX Designer</h4>
+        <h3>{props.company}<span>{props.date}</span></h3>
+        <h4>{props.post}</h4>
         <div id='partse' >
-          <h4>Part-Time</h4>
-          <h4>Senior Level</h4>
+          <h4>{props.tag1}</h4>
+          <h4>{props.tag2}</h4>
           
         </div>
       </div>
       </div>
       <div className='buttom'>
         <div className='left'>
-        <h3>$120/hr</h3>
-        <p>Chitwan, Nepal</p>
+        <h3>{props.salary}</h3>
+        <p>{props.location}</p>
         </div>
         <button>Apply Now</button>
       </div>
