72bdeb81fd072690e2c3aa084048a3852f43d305
sandhyashrestha73 <sandhyashrestha431@gmail.com>
2026-05-09 07:17:36 +0545

    ProjectProp

--- 04-Componentpropsproject/src/App.jsx
+++ 04-Componentpropsproject/src/App.jsx
@@ -1,16 +1,18 @@
+
 import React from 'react'
-import {Bookmark} from 'lucide-react'
+
+
 const App = () => {
   return (
     <div className="parent">
       <div className="card">
       <div className='top'>
           <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRCw9ADW_e3phAevjk1wGYCyOujxGmT_3cTQQ&s" alt="" />
-          <button>Save <Bookmark /></button>
+          <button>Save </button>
       </div>
       <div className='center'>
         <h3>Amazon <span>5 days ago</span></h3>
-        <h3>Senior UI/UX Designer</h3>
+        <h4>Senior UI/UX Designer</h4>
         <div>
           <h4>Part Time</h4>
           <h4>Senior Level</h4>
@@ -19,7 +21,7 @@ const App = () => {
       <div className='buttom'>
         <h3>$120/hr</h3>
         <p>Chitwan, Nepal</p>
-
+        <button>Apply Now</button>
       </div>
       </div>
     </div>
